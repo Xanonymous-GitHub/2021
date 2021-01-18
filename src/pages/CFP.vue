10f86d6049ae92cc42cc77425277bf8a4ed6ba53
barrystone <barrystone3181@gmail.com>
2021-01-18 17:07:50 +0800

    [build] basic destop screen

--- src/pages/CFP.vue
+++ src/pages/CFP.vue
@@ -1,13 +1,20 @@
 <template>
   <div class="cfp">
-    <h1>cfp</h1>
+    <!-- <h1>cfp</h1> -->
+    <Agenda />
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
+import { Component, Prop, Vue } from "vue-property-decorator";
 
-@Component({})
+import Agenda from "../components/CFP/Agenda.vue";
+
+@Component({
+  components: {
+    Agenda,
+  },
+})
 export default class CFP extends Vue {
   @Prop() private msg!: string;
 }
