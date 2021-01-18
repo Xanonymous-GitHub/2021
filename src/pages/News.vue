791c65a94763bf7f8e6470ed4698f4b38fc504d1
mysper <anthony.a3203@gmail.com>
2021-01-19 04:59:36 +0800

    [Merge] merge the schedule and Agenda

--- src/pages/News.vue
+++ src/pages/News.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="news">
       <NewsHeader id="news-header" />
-      <h1>news</h1>
+      <Schedule/>
+      <Agenda/>
     <!-- <Footer></Footer> should be put at App.vue (?) -->
   </div>
 </template>
@@ -9,8 +10,16 @@
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
 import NewsHeader from '@/components/NewsHeader.vue';
+import Schedule from '../components/CFP/Schedule.vue';
+import Agenda from '../components/CFP/Agenda.vue';
 
-@Component({ components: { NewsHeader } })
+@Component({
+  components: {
+    NewsHeader,
+    Schedule,
+    Agenda
+  }
+})
 export default class News extends Vue {
   @Prop() private msg!: string;
 }
